b394ecdd8b2017cea7695c2f8db042de1aafbeb1
Marcos Huerta <marcos@marcoshuerta.com>
2023-08-13 11:14:41 -0400

    improvements maybe a panel

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from shiny import App, render, ui
+from shiny import App, render, ui, experimental
 from EmojiFinder import EmojiFinderSql
 
 e = EmojiFinderSql()
@@ -6,19 +6,59 @@ e = EmojiFinderSql()
 app_ui = ui.page_fluid([
     ui.panel_title("Emoji Search for Shiny!"),
     ui.input_text("search", "Search", placeholder="Search emoji"),
-    ui.output_table("txt"),
+    ui.output_ui("txt"),
 ])
+
+
+def process_additional_emojis(item):
+    additional_emojis = e.add_variants(item['label'])
+    return [{
+        'emoji': e.emoji_dict[x]['emoji'],
+        'text': e.emoji_dict[x]['text'],
+        'label': x
+    } for x in additional_emojis]
+
+
+def process_one_emoji(row):
+    more_emojis = process_additional_emojis(row)
+    if not more_emojis:
+        return row['emoji']
+    else:
+        items = [
+            experimental.ui.accordion_panel(x['emoji']) for x in more_emojis
+        ]
+        return experimental.ui.accordion(*items,
+                                         open=False,
+                                         id='myaccordion')
+
+
+def make_row(row, i):
+    return ui.tags.tr([
+        ui.tags.td(row['text']),
+        ui.tags.td(process_one_emoji(row)),
+    ])
+
+
+def make_my_table(rows):
+    out = ui.tags.table(
+        {'class': 'table w-50 table-striped'},
+        ui.tags.thead(ui.tags.tr([ui.tags.th("Label"),
+                                  ui.tags.th("Emoji")])),
+        ui.tags.tbody([make_row(x, i) for i, x in enumerate(rows)]),
+    )
+    #print(out)
+    return out
 
 
 def server(input, output, session):
 
     @output
-    @render.table
+    @render.ui
     def txt():
-        out = e.top_emojis(input.search())
+        out = e.top_emojis(input.search()).to_dict(orient='records')
         print(type(out))
-        if out is not None and not out.empty:
-            return out[['text', 'emoji']]
+        if out:
+            return make_my_table(out)
         return None
 
 
