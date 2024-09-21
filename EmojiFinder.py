898fddff6793c5c7006305d4e35a706e678d1a99
Marcos Huerta <marcos@marcoshuerta.com>
2024-09-21 13:11:52 -0400

    support version 15.1

--- EmojiFinder.py
+++ EmojiFinder.py
@@ -14,24 +14,20 @@ def flatten_list(list_of_lists):
     return [y for x in list_of_lists for y in x]
 
 
-class EmojiFinderCached():
-
+class EmojiFinderCached:
     def __init__(self, model_name='all-mpnet-base-v2'):
         self.emoji_df = pd.read_parquet('emoji_df_improved.parquet')
         self.emoji_dict = self.emoji_df.set_index('label')['emoji'].to_dict()
-        self.emoji_text_dict = self.emoji_df.set_index(
-            'label')['text'].to_dict()
+        self.emoji_text_dict = self.emoji_df.set_index('label')['text'].to_dict()
         vocab_df = pd.read_parquet(f'vocab_df_{model_name}.parquet')
         self.vocab_dict = vocab_df.set_index('word')['idx'].to_dict()
-        self.distances = pd.read_parquet(
-            f'semantic_distances_{model_name}.parquet').values
+        self.distances = pd.read_parquet(f'semantic_distances_{model_name}.parquet').values
 
     def filter_list(self, list1):
-        return sorted(
-            list(set(list1).intersection(self.emoji_df['label'].tolist())))
+        return sorted(list(set(list1).intersection(self.emoji_df['label'].tolist())))
 
     def add_variants(self, base_label):
-        #print(base_label)
+        # print(base_label)
         base_search = base_label[1:-1]
         if base_search in SKIN_TONE_SUFFIXES:
             return []
@@ -40,49 +36,55 @@ class EmojiFinderCached():
                 base_search = base_search.replace(prefix, '')
                 # print(f'new base {base_search}')
                 break
-        variants = [f":{base_search}_{x}:" for x in SKIN_TONE_SUFFIXES]
-        #print(variants)
-        man_variants = [':man_' + base[1:]
-                        for base in variants] + [f':man_{base_search}:']
-        woman_variants = [':woman_' + base[1:]
-                          for base in variants] + [f':woman_{base_search}:']
-        person_variants = [':person_' + base[1:]
-                           for base in variants] + [f':person_{base_search}:']
+        variants = [f':{base_search}_{x}:' for x in SKIN_TONE_SUFFIXES]
+        # print(variants)
+        man_variants = [':man_' + base[1:] for base in variants] + [f':man_{base_search}:']
+        woman_variants = [':woman_' + base[1:] for base in variants] + [
+            f':woman_{base_search}:'
+        ]
+        person_variants = [':person_' + base[1:] for base in variants] + [
+            f':person_{base_search}:'
+        ]
 
         extra_suffixes = flatten_list(
-            [[f":{gender}_{x}_{base_search}:" for x in SKIN_TONE_SUFFIXES]
-             for gender in ['man', 'woman', 'person']])
+            [
+                [f':{gender}_{x}_{base_search}:' for x in SKIN_TONE_SUFFIXES]
+                for gender in ['man', 'woman', 'person']
+            ]
+        )
 
         #        print(len(variants))
-        return self.filter_list(variants) + self.filter_list(
-            woman_variants) + self.filter_list(
-                man_variants) + self.filter_list(
-                    person_variants) + self.filter_list(extra_suffixes)
+        return (
+            self.filter_list(variants)
+            + self.filter_list(woman_variants)
+            + self.filter_list(man_variants)
+            + self.filter_list(person_variants)
+            + self.filter_list(extra_suffixes)
+        )
 
     def top_emojis(self, search):
         search = search.strip().lower()
-        if (idx := self.vocab_dict.get(search)):
-            return self.emoji_df.iloc[(
-                self.distances[idx])].query('version <= 15.0')
+        if idx := self.vocab_dict.get(search):
+            return self.emoji_df.iloc[(self.distances[idx])].query('version <= 15.0')
         else:
             return pd.DataFrame(columns=['text', 'emoji'])
 
 
 class EmojiFinderSql(EmojiFinderCached):
-
     def __init__(self, db_name='all-mpnet-base-v2_main.db'):
         self.db_name = db_name
         print('Begin init of class')
-        #self.con = sqlite3.connect(
+        # self.con = sqlite3.connect(
         #    'main.db')  #change later, name should have model type in it
 
-        #self.base_emoji_map = self.make_variant_map()
+        # self.base_emoji_map = self.make_variant_map()
 
         print('end init of class')
 
     def prep_for_variant_map(self):
-        self.all_labels = pd.read_sql('select distinct label from emoji_df;',
-                                      con=self.con)['label'].tolist()
+        self.all_labels = pd.read_sql('select distinct label from emoji_df;', con=self.con)[
+            'label'
+        ].tolist()
         self.make_variant_map()
 
     def sql_frame(self, query, params=None):
@@ -93,14 +95,19 @@ class EmojiFinderSql(EmojiFinderCached):
         # print(df.shape)
         # return df
         return dict(
-            zip(['idx', 'emoji', 'label', 'version', 'text', 'base_emoji'],
-                self.con.execute("Select * from emoji_df where label = ?;",
-                                 (label, )).fetchone()))
+            zip(
+                ['idx', 'emoji', 'label', 'version', 'text', 'base_emoji'],
+                self.con.execute(
+                    'Select * from emoji_df where label = ?;', (label,)
+                ).fetchone(),
+            )
+        )
 
     def sql_add_variants(self, label):
         return self.sql_frame(
-            "select label from emoji_df where base_emoji = ? and base_emoji <> label",
-            params=(label, ))['label'].to_list()
+            'select label from emoji_df where base_emoji = ? and base_emoji <> label',
+            params=(label,),
+        )['label'].to_list()
 
     @property
     def con(self):
@@ -119,14 +126,13 @@ class EmojiFinderSql(EmojiFinderCached):
         self.base_emoji_map = new_dict.copy()
 
     def top_emojis(self, search):
-
         search = search.strip().lower()
         results = pd.read_sql(
-            "select * from combined_emoji where word = ?  and label = base_emoji order by rank_of_search;",
+            'select * from combined_emoji where word = ?  and label = base_emoji order by rank_of_search;',
             con=self.con,
-            params=(search, ))
+            params=(search,),
+        )
         if not results.empty:
-            return results.query(
-                'version <= 15.0')  ## move this into sql and add index?
+            return results.query('version <= 15.1')  ## move this into sql and add index?
         else:
             return pd.DataFrame(columns=['text', 'emoji'])
--- precompute.py
+++ precompute.py
@@ -11,11 +11,9 @@ import duckdb
 class ComputeDistances:
     distance_df = None
 
-    def __init__(self,
-                 model_name='all-mpnet-base-v2',
-                 emoji_prefix='',
-                 vocab_prefix='',
-                 vocab_suffix='') -> None:
+    def __init__(
+        self, model_name='all-mpnet-base-v2', emoji_prefix='', vocab_prefix='', vocab_suffix=''
+    ) -> None:
         self.model_name = model_name
         self.emoji_prefix = emoji_prefix
         self.vocab_prefix = vocab_prefix
@@ -23,25 +21,22 @@ class ComputeDistances:
         self.emoji_data = pd.read_parquet(
             'emoji_df_improved.parquet'
         )  # dataframe of emojis and their descriptions
-        if torch.backends.mps.is_available(
-        ):  #use metal back end if available (on an Apple Silicon Mac)
-            #I probably should add a similar hook  for CUDA but this is fast enough it doesn't really need CUDA
-            self.model = SentenceTransformer(model_name,
-                                             device=torch.device('mps'),
-                                             trust_remote_code=True)
+        if (
+            torch.backends.mps.is_available()
+        ):  # use metal back end if available (on an Apple Silicon Mac)
+            # I probably should add a similar hook  for CUDA but this is fast enough it doesn't really need CUDA
+            self.model = SentenceTransformer(
+                model_name, device=torch.device('mps'), trust_remote_code=True
+            )
         else:
             self.model = SentenceTransformer(model_name)
-        self.all_words = pd.read_csv(
-            'cleaned_wordlist_allv2.txt', header=None
-        )[0].dropna().tolist(
+        self.all_words = (
+            pd.read_csv('cleaned_wordlist_allv2.txt', header=None)[0].dropna().tolist()
         )  ## this list has not been lemmatized, but de-duplicated already. Lemmatization at runtime is too memory intensive with nltk
 
     def make_emoji_vectors(self):
         emoji_dict = self.emoji_data.set_index('label')['text'].to_dict()
-        no_variants = [
-            x for x in self.emoji_data['label'].to_list()
-            if 'skin_tone' not in x
-        ]
+        no_variants = [x for x in self.emoji_data['label'].to_list() if 'skin_tone' not in x]
         #  no_variants = [x for x in no_variants if not (x.startswith(':woman'))]
         # no_variants = [x for x in no_variants ]
         # no_variants = no_variants + [':man:', ':woman:']
@@ -55,32 +50,34 @@ class ComputeDistances:
         map_index_orig = {key: i for i, key in enumerate(emoji_dict.keys())}
         map_index_limited = {i: key for i, key in enumerate(no_variants)}
         self.index_to_index = {
-            i: map_index_orig[map_index_limited[i]]
-            for i in range(len(no_variants))
+            i: map_index_orig[map_index_limited[i]] for i in range(len(no_variants))
         }
 
     def make_vocab_vectors(self, n=40000):
         vocab = self.all_words[:n]
         self.vector_array_search_terms = self.model.encode(
-            [self.vocab_prefix + x + self.vocab_suffix for x in vocab])
+            [self.vocab_prefix + x + self.vocab_suffix for x in vocab]
+        )
         self.vocab_df = pd.DataFrame(pd.Series(vocab)).reset_index()
         self.vocab_df.columns = ['idx', 'word']
 
     def make_all(self):
-        print("making emoji vectors")
+        print('making emoji vectors')
         self.make_emoji_vectors()
-        print("making vocab dataframe")
+        print('making vocab dataframe')
         self.make_vocab_vectors()
         print('computing distances')
         self.precompute_distances()
 
     def precompute_distances(self, method='cos', n=25):
         if method != 'cos':
-            distances = util.dot_score(self.vector_array_search_terms,
-                                       self.vector_array_emoji).numpy()
+            distances = util.dot_score(
+                self.vector_array_search_terms, self.vector_array_emoji
+            ).numpy()
         else:
-            distances = util.cos_sim(self.vector_array_search_terms,
-                                     self.vector_array_emoji).numpy()
+            distances = util.cos_sim(
+                self.vector_array_search_terms, self.vector_array_emoji
+            ).numpy()
 
         top_n = np.argsort(-distances)[:, 0:n]
         self.distance_df = pd.DataFrame(top_n)
@@ -89,57 +86,55 @@ class ComputeDistances:
     def save_all(self):
         self.distance_df.to_parquet(
             f'semantic_distances_{self.model_name}.parquet'
-        )  #precomputed top 25 indices of matching emojis
+        )  # precomputed top 25 indices of matching emojis
         self.vocab_df.to_parquet(
             f'vocab_df_{self.model_name}.parquet'
         )  # matches indices to the emoji itself (could combine into a single lookup dict)
         self.vector_array_emoji_df.to_parquet(
-            f"emoji_vectors_{self.model_name}.parquet"
+            f'emoji_vectors_{self.model_name}.parquet'
         )  ## needed to speed up live encoding
         ## of the search terms, only need to model.encode(search).
 
     def save_emoji_vectors_only(self):
-        self.distance_df.to_parquet(
-            f'semantic_distances_{self.model_name}.parquet')
+        self.distance_df.to_parquet(f'semantic_distances_{self.model_name}.parquet')
         self.distance_df.to_parquet(f'vocab_df_{self.model_name}.parquet')
 
     def make_database(self, db_name=None):
         """Need to test this!"""
         if not db_name:
-            db_name = f"{self.model_name}_main.db"
-        con = create_engine(f"sqlite:///{db_name}")
+            db_name = f'{self.model_name}_main.db'
+        con = create_engine(f'sqlite:///{db_name}')
         self.distance_df.index = self.vocab_df['word']
         new_df = self.distance_df.T
         new_df.index.name = 'rank_of_search'
-        melted_df = pd.melt(new_df.reset_index(),
-                            value_name='word_lookup',
-                            id_vars=['rank_of_search'])
-        melted_df['word_lookup'] = melted_df['word_lookup'].map(
-            self.index_to_index)
+        melted_df = pd.melt(
+            new_df.reset_index(), value_name='word_lookup', id_vars=['rank_of_search']
+        )
+        melted_df['word_lookup'] = melted_df['word_lookup'].map(self.index_to_index)
         melted_df['rank_of_search'] = melted_df['rank_of_search'].astype(int)
         melted_df.to_sql('lookup', con=con, index=False, if_exists='replace')
-        self.emoji_data.reset_index().rename(columns={
-            'index': 'idx'
-        }).to_sql('emoji_df', con=con, index=False, if_exists='replace')
+        self.emoji_data.reset_index().rename(columns={'index': 'idx'}).to_sql(
+            'emoji_df', con=con, index=False, if_exists='replace'
+        )
 
 
 def make_duckb_vectors(model_name):
     c = ComputeDistances(model_name)
     c.make_emoji_vectors()
     array_list = c.vector_array_emoji_df.values.tolist()
-    con = duckdb.connect(f'{model_name}_vectors.db')
+    con = duckdb.connect('vectors.db')
     con.sql('CREATE or replace TABLE array_table (id INT, arr double[384])')
     for i in range(len(array_list)):
-        sql = f"insert into array_table values({c.index_to_index[i]},{array_list[i]});"
+        sql = f'insert into array_table values({c.index_to_index[i]},{array_list[i]});'
         con.sql(sql)
-    con.sql("create index array_id on array_table(id);")
+    con.sql('create index array_id on array_table(id);')
     con.commit()
     con.close()
 
 
 if __name__ == '__main__':
-
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('model_name')
 
@@ -148,3 +143,4 @@ if __name__ == '__main__':
     c = ComputeDistances(args.model_name)
     c.make_all()
     c.make_database()
+    make_duckb_vectors('all-MiniLM-L6-v2')
